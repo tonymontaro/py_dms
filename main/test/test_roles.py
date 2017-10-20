957c4ea2d89cde7f0ac114569d6ae0e2e6686341
Anthony Ngene <anthony.ngene@andela.com>
2017-10-20 14:16:19 +0100

    chore(test): refactor roles test

--- main/test/test_roles.py
+++ main/test/test_roles.py
@@ -5,79 +5,84 @@ from rest_framework.test import APIClient
 from rest_framework import status
 from django.core.urlresolvers import reverse
 
-
-# Create your tests here.
-
-
-class ModelTestCase(TestCase):
-    """Defines the test suite for the ROLE model."""
-
-    def setUp(self):
-        user = User.objects.create(username='nerd')
-        self.role_name = 'editor'
-        self.role = Role(name=self.role_name)
-
-    def test_model_can_create_a_role(self):
-        old_count = Role.objects.count()
-        self.role.save()
-        new_count = Role.objects.count()
-        self.assertEqual(new_count, old_count + 1)
+import json
 
 
 class ViewTestCase(TestCase):
     """Test suite for the api views"""
 
-    def setUp(self):
-        User.objects.create(username='nerd')
-        admin_user = User.objects.create_superuser(username='boss', email='boss@g.com', password='password')
-        self.client = APIClient()
-        self.client.force_authenticate(user=admin_user)
+    @classmethod
+    def setUpClass(cls):
+        admin = Role.objects.create(name='admin')
+        regular = Role.objects.create(name='regular')
 
-        self.role_data = {'name': 'reviewer'}
-        self.response = self.client.post(
-            reverse('role_list_create'),
-            self.role_data,
-            format='json'
+        cls.admin_user = User.objects.create_superuser(
+            username='admin-user',
+            email='boss@g.com',
+            password='password',
+            role_id=admin
+        )
+        cls.regular_user = User.objects.create(
+            username='reg-user',
+            email='boss@g.com',
+            password='password',
+            role_id=regular
         )
 
-    def test_api_can_create_a_role(self):
+    def setUp(self):
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.admin_user)
+
+    def test_01_api_can_create_a_role(self):
         """Api can create a role"""
-        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+        res = self.client.post(
+            reverse('role_list_create'),
+            {'name': 'editor'},
+            format='json',
+        )
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(json.loads(res.content)['name'], 'editor')
 
-    def test_authorization_is_enforced(self):
+    def test_02_authorization_is_enforced(self):
         """Authorization is enforced"""
-        new_client = APIClient()
-        res = new_client.post('/roles', {'name': 'loafers'}, format='json')
-        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.client.force_authenticate(user=self.regular_user)
+        res = self.client.post('/roles', {'name': 'loafers'}, format='json')
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
 
-    def test_api_can_get_a_role(self):
+    def test_03_should_fail_if_role_exists(self):
+        res = self.client.post('/roles', {'name': 'regular'}, format='json')
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(json.loads(res.content)['name'][0], 'role with this name already exists.')
+
+    def test_03_api_can_get_a_role(self):
         """Api can get a role"""
-        role = Role.objects.first()
         response = self.client.get(
             '/roles/',
-            kwargs={'pk': role.id},
+            kwargs={'pk': 1},
             format='json'
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertContains(response, role)
+        self.assertContains(response, Role.objects.get(name='admin'))
 
-    def test_api_can_update_role(self):
+    def test_04_api_can_update_role(self):
         """"Api can update role"""
-        role = Role.objects.first()
         res = self.client.put(
-            reverse('role_details', kwargs={'pk': role.id}),
-            {'name': 'Something_new'},
+            reverse('role_details', kwargs={'pk': 2}),
+            {'name': 'new_role'},
             format='json'
         )
         self.assertEqual(res.status_code, status.HTTP_200_OK)
-        self.assertJSONEqual(res.content, {'id': role.id, 'name': 'Something_new'})
+        self.assertJSONEqual(res.content, {'id': 2, 'name': 'new_role'})
 
-    def test_api_can_delete_role(self):
+    def test_05_api_can_delete_role(self):
         """Api can delete role"""
-        role = Role.objects.get()
         response = self.client.delete(
-            reverse('role_details', kwargs={'pk': role.id}),
+            reverse('role_details', kwargs={'pk': 2}),
             format='json',
             follow=True
         )
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
