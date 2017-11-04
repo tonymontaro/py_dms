07d1d6c0ad2da27b3b89ce4e220cefe967beec62
Anthony Ngene <anthony.ngene@andela.com>
2017-11-04 01:58:56 +0100

    chore(edit): add edit button on the document page

--- src/client/components/documents/DocumentPage.jsx
+++ src/client/components/documents/DocumentPage.jsx
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
 import renderHTML from 'react-render-html';
 import { getDocument } from '../../actions/documentActions';
 
@@ -22,13 +23,12 @@ class DocumentPage extends React.Component {
   * @returns {Undefined} nothing
   */
   componentWillMount() {
-    this.props.getDocument(this.props.params.id)
-      .catch((error) => {
-        if (error.response) {
-          Materialize.toast('Invalid document id', 2000);
-        }
-        this.context.router.push('/');
-      });
+    this.props.getDocument(this.props.params.id).catch((error) => {
+      if (error.response) {
+        Materialize.toast('Invalid document id', 2000);
+      }
+      this.context.router.push('/');
+    });
   }
 
   /**
@@ -37,16 +37,30 @@ class DocumentPage extends React.Component {
   * @returns {Object} jsx object
   */
   render() {
-    const { document } = this.props;
+    const { document, user } = this.props;
 
     return (
       <div className="document-div">
         <div className="document container">
           <h3>{document.title}</h3>
-          <p className="meta-info">posted on: {new Date(document.createdAt).toDateString()},
-            by: <span className="teal-text">{document.user ? document.user.username : ''}</span></p>
+          <p className="meta-info">
+            posted on: {new Date(document.createdAt).toDateString()}, by:{' '}
+            <span className="teal-text">{document.user ? document.user.username : ''}</span>
+          </p>
           <div>{document.content && renderHTML(document.content)}</div>
         </div>
+
+        {user &&
+          user.id === document.authorId && (
+            <div className="container">
+              <Link
+                to={`document/${document.id}`}
+                className="btn-floating waves-effect waves-light right edit-btn"
+              >
+                <i className="material-icons">edit</i>
+              </Link>
+            </div>
+          )}
       </div>
     );
   }
@@ -55,11 +69,14 @@ class DocumentPage extends React.Component {
 DocumentPage.propTypes = {
   document: PropTypes.object.isRequired,
   getDocument: PropTypes.func.isRequired,
-  params: PropTypes.object
+  params: PropTypes.object,
+  user: PropTypes.object.isRequired,
 };
 
 DocumentPage.contextTypes = {
   router: PropTypes.object.isRequired,
 };
 
-export default connect(state => ({ document: state.document }), { getDocument })(DocumentPage);
+export default connect(state => ({ document: state.document, user: state.access.user }), {
+  getDocument,
+})(DocumentPage);
